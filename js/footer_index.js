e697dcdcde3e6841033816d2d5c160bd30228a56
Paulo Pissinini <pissininineto8@gmail.com>
2025-12-16 15:44:56 -0300

    Ajustes avisos

--- js/footer_index.js
+++ js/footer_index.js
@@ -299,7 +299,7 @@ const rodape=
     "<br><br>"+
 "</div>" +
 "</div>" +
-"<p id='versao'>Vers&atilde;o 0.1.11</p>"+
+"<p id='versao'>Vers&atilde;o 0.1.12</p>"+
 "</article>"+
 "</footer>"
 foot.innerHTML+=rodape
