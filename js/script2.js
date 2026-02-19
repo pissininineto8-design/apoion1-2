4cb5b486f0e76e60806bbe39af96df87bd9c252d
Paulo Pissinini <pissininineto8@gmail.com>
2026-02-19 15:21:30 -0300

    Ajustes templates

--- js/script2.js
+++ js/script2.js
@@ -1,189 +1,194 @@
 
-    function leiaMais(){
-      var mostrar = document.getElementById("mostrar");
-      var maisTexto = document.getElementById("mais");
-      var btnLeiaMais = document.getElementById("btnLeiaMais");
+function leiaMais(){
+var mostrar = document.getElementById("mostrar");
+var maisTexto = document.getElementById("mais");
+var btnLeiaMais = document.getElementById("btnLeiaMais");
 
-      if(mostrar.style.display === "none"){
-        mostrar.style.display = "inline";
-      } else {
-        maisTexto.style.display = "inline";
-    }
-    }
+if(mostrar.style.display === "none"){
+mostrar.style.display = "inline";
+} else {
+maisTexto.style.display = "inline";
+}
+}
 
-      function myFunction() {
-      var textoCopiado = document.getElementById("txtInput");
+function myFunction() {
+var textoCopiado = document.getElementById("txtInput");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction2() {
-      var textoCopiado = document.getElementById("txtInput2");
+function myFunction2() {
+var textoCopiado = document.getElementById("txtInput2");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction3() {
-      var textoCopiado = document.getElementById("txtInput3");
+function myFunction3() {
+var textoCopiado = document.getElementById("txtInput3");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction4() {
-      var textoCopiado = document.getElementById("txtInput4");
+function myFunction4() {
+var textoCopiado = document.getElementById("txtInput4");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction5() {
-      var textoCopiado = document.getElementById("txtInput5");
+function myFunction5() {
+var textoCopiado = document.getElementById("txtInput5");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction6() {
-      var textoCopiado = document.getElementById("txtInput6");
+function myFunction6() {
+var textoCopiado = document.getElementById("txtInput6");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction7() {
-      var textoCopiado = document.getElementById("txtInput7");
+function myFunction7() {
+var textoCopiado = document.getElementById("txtInput7");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction8() {
-      var textoCopiado = document.getElementById("txtInput8");
+function myFunction8() {
+var textoCopiado = document.getElementById("txtInput8");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction9() {
-      var textoCopiado = document.getElementById("txtInput9");
+function myFunction9() {
+var textoCopiado = document.getElementById("txtInput9");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction10() {
-      var textoCopiado = document.getElementById("txtInput10");
+function myFunction10() {
+var textoCopiado = document.getElementById("txtInput10");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction11() {
-      var textoCopiado = document.getElementById("txtInput11");
+function myFunction11() {
+var textoCopiado = document.getElementById("txtInput11");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction12() {
-      var textoCopiado = document.getElementById("txtInput12");
+function myFunction12() {
+var textoCopiado = document.getElementById("txtInput12");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction13() {
-      var textoCopiado = document.getElementById("txtInput13");
+function myFunction13() {
+var textoCopiado = document.getElementById("txtInput13");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction14() {
-      var textoCopiado = document.getElementById("txtInput14");
+function myFunction14() {
+var textoCopiado = document.getElementById("txtInput14");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction15() {
-      var textoCopiado = document.getElementById("txtInput15");
+function myFunction15() {
+var textoCopiado = document.getElementById("txtInput15");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction16() {
-      var textoCopiado = document.getElementById("txtInput16");
+function myFunction16() {
+var textoCopiado = document.getElementById("txtInput16");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
 
-      function myFunction17() {
-      var textoCopiado = document.getElementById("txtInput17");
+function myFunction17() {
+var textoCopiado = document.getElementById("txtInput17");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction18() {
-      var textoCopiado = document.getElementById("txtInput18");
+document.execCommand("Copy");
+}
+function myFunction18() {
+var textoCopiado = document.getElementById("txtInput18");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction19() {
-      var textoCopiado = document.getElementById("txtInput19");
+document.execCommand("Copy");
+}
+function myFunction19() {
+var textoCopiado = document.getElementById("txtInput19");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction20() {
-      var textoCopiado = document.getElementById("txtInput20");
+document.execCommand("Copy");
+}
+function myFunction20() {
+var textoCopiado = document.getElementById("txtInput20");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction21() {
-      var textoCopiado = document.getElementById("txtInput21");
+document.execCommand("Copy");
+}
+function myFunction21() {
+var textoCopiado = document.getElementById("txtInput21");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction22() {
-      var textoCopiado = document.getElementById("txtInput22");
+document.execCommand("Copy");
+}
+function myFunction22() {
+var textoCopiado = document.getElementById("txtInput22");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction23() {
-      var textoCopiado = document.getElementById("txtInput23");
+document.execCommand("Copy");
+}
+function myFunction23() {
+var textoCopiado = document.getElementById("txtInput23");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction24() {
-      var textoCopiado = document.getElementById("txtInput24");
+document.execCommand("Copy");
+}
+function myFunction24() {
+var textoCopiado = document.getElementById("txtInput24");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction25() {
-      var textoCopiado = document.getElementById("txtInput25");
+document.execCommand("Copy");
+}
+function myFunction25() {
+var textoCopiado = document.getElementById("txtInput25");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction26() {
-      var textoCopiado = document.getElementById("txtInput26");
+document.execCommand("Copy");
+}
+function myFunction26() {
+var textoCopiado = document.getElementById("txtInput26");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction27() {
-      var textoCopiado = document.getElementById("txtInput27");
+document.execCommand("Copy");
+}
+function myFunction27() {
+var textoCopiado = document.getElementById("txtInput27");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction28() {
-      var textoCopiado = document.getElementById("txtInput28");
+document.execCommand("Copy");
+}
+function myFunction28() {
+var textoCopiado = document.getElementById("txtInput28");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction29() {
-      var textoCopiado = document.getElementById("txtInput29");
+document.execCommand("Copy");
+}
+function myFunction29() {
+var textoCopiado = document.getElementById("txtInput29");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction30() {
-      var textoCopiado = document.getElementById("txtInput30");
+document.execCommand("Copy");
+}
+function myFunction30() {
+var textoCopiado = document.getElementById("txtInput30");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction31() {
-      var textoCopiado = document.getElementById("txtInput31");
+document.execCommand("Copy");
+}
+function myFunction31() {
+var textoCopiado = document.getElementById("txtInput31");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
-      function myFunction32() {
-      var textoCopiado = document.getElementById("txtInput32");
+document.execCommand("Copy");
+}
+function myFunction32() {
+var textoCopiado = document.getElementById("txtInput32");
 textoCopiado.select();
-    document.execCommand("Copy");
-      }
+document.execCommand("Copy");
+}
+function myFunction33() {
+var textoCopiado = document.getElementById("txtInput33");
+textoCopiado.select();
+document.execCommand("Copy");
+}
